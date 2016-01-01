2df3ac686d940a5fe59f18e2e0c152b707df7cae
surajfuturenet <suraj145@gmail.com>
2016-01-01 11:59:57 +0530

    After change

--- Employee/Employee/view/Login.aspx.designer.cs
+++ Employee/Employee/view/Login.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace Employee.view {
     
     
     public partial class WebForm2 {
+        
+        /// <summary>
+        /// UnameTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox UnameTextBox;
+        
+        /// <summary>
+        /// login control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button login;
     }
 }
