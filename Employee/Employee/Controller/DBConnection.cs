2833b5c20771bbf183242c79d85f1eb6732ad471
suraj <suraj145@gmail.com>
2016-01-03 18:33:52 +0530

    solution

--- Employee/Employee/Controller/DBConnection.cs
+++ Employee/Employee/Controller/DBConnection.cs
@@ -2,39 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Data.Sql;
-using System.Data.SqlClient;
-using System.Configuration;
 
 namespace Employee.Controller
 {
     public class DBConnection
     {
-        public SqlConnection connection;
-        public DBConnection() {
 
-            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeDBConnectionString"].ConnectionString);
-            
-            
-
-        }
-
-        public SqlConnection CreateConnection() {
-
-            try
-            {
-                connection.Open();
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Connection failed");
-            }
-            return connection;
-        }
-
-        public void CloseConnection()
-        {
-            connection.Close();
-        }
     }
 }
