4261da2e01be5918918a541edf1bfddf6dab1115
surajfuturenet <suraj145@gmail.com>
2016-01-04 17:03:39 +0530

    UserManagement DashBoard

--- Employee/Employee/view/UserManagement.aspx.cs
+++ Employee/Employee/view/UserManagement.aspx.cs
@@ -11,12 +11,38 @@ namespace Employee.view
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           UserName.Text = "User Name";
-           FirstName.Text = "First Name";
-           LastName.Text = "Last Name";
-           Email.Text = "Email";
 
-            User.Text = "User";
+            int UserTYpe = 0; // retrieve the user type
+            User.Text = "User Name";
+            if(UserTYpe == 1)   // for Nomal user
+            {
+                userManagement.Visible= false;
+                tabs_2.Visible = false;
+            }
+
+            if (UserTYpe == 0)  // for Admin
+            {
+                userManagement.Visible = true;
+                //tabs_2.Visible = true;
+            }
+
+            //User.Text = "User";
+
+            EmailTextBox.ReadOnly = true;
+            UnameTextBox.ReadOnly = true;
+
+            // get the User and Update
+           
+
+        }
+
+        protected void update_Click(object sender, EventArgs e)
+        {
+            if (RegularExpressionValidator1.Text.Equals(""))
+            {
+                // Update the profile
+                error.Text = "Data Sccussfully Updated";
+            }
         }
     }
 }
